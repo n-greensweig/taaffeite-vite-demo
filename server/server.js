90a630412a9276a725b87fab265ab1c610380a04
Noah Greensweig <ngreensweig@gmail.com>
2024-01-24 10:57:45 -0600

    Added server to Vite project

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exampleRouter = require('./routes/example.router');
+const app = express();
+const PORT = 5009;
+
+// Vite will put all static files into a 
+// build folder when deployed to Heroku.
+app.use(express.static('dist'));
+// Required for req.body
+app.use(express.json());
+
+// Routes will go here
+// Routes must be prefixed in Vite
+app.use('/api/example', exampleRouter);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('Listening on port:',  PORT);
+});
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": 'http://localhost:5009'
+    }
+  },
   plugins: [react()],
 })
